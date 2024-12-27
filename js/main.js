ccf37e535c7f9f86cf118ff056aacbd2fd2d6d29
Your Name <abozaidkhaledmahmoud@gmail.com>
2024-12-27 15:14:59 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//   navbar function 
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(Window).scrollTop()  >  30){
+            $('header').addClass('header-active');
+        }else{
+            $('header').removeClass('header-active');
+        }
+    });
+
+    
+});
+
+
+$(document).ready(function () {
+    $("#homeButton").on("click", function () {
+      window.location.href = "/html/doctor.html"; // تغيير المسار إلى الصفحة المطلوبة
+    });
+  });
+
+
+
+
+
+
+
